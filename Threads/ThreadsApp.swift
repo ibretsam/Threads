2dd6771aa07df1f6bdcdff2ae9cea4397d9b5121
Khanh Le <masterbruculous@gmail.com>
2024-07-07 22:44:02 +0700

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by MasterBi on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
